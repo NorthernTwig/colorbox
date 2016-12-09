3de842542a0f0877e69dd5bb776ace992387af47
Oscar Nordquist <on222bx@student.lnu.se>
2016-12-09 20:43:34 +0100

    #magic

---
+++ client/src/FancyCanvas.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react'
+import {EventEmitter} from 'fbemitter'
+import './FancyCanvas.css'
+const emitter = new EventEmitter()
+
+export default class FancyCanvas extends Component {
+  constructor() {
+    super()
+    this.state = {
+      context: null,
+      flowRight: false,
+      flowLeft: true
+    }
+  }
+
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext('2d')
+    this.setState({context: ctx})
+    this.setListener()
+  }
+
+  showImage = (e) => {
+    const ctx = this.state.context
+    const reader = new FileReader()
+    reader.onload = (ev) => {
+      const img = new Image()
+      img.onload = (e) => {
+        ctx.drawImage(
+          img,
+          0,
+          0,
+          img.width,
+          img.height,
+          0,
+          0,
+          ctx.canvas.width,
+          ctx.canvas.height)
+          emitter.emit('loaded')
+        }
+        img.src = ev.target.result
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+  setListener() {
+    emitter.addListener('loaded', () => {
+
+      let r = 0
+      let g = 0
+      let b = 0
+      let count = 0
+
+      for (let x = 0; x < this.state.context.canvas.clientWidth; x++) {
+        for (let y = 0; y < this.state.context.canvas.clientHeight; y++) {
+          const pixelData = this.state.context.getImageData(x, y, 1, 1)
+
+          r += pixelData.data[0]
+          g += pixelData.data[1]
+          b += pixelData.data[2]
+          count++
+
+        }
+      }
+
+      r = Math.floor(r / count)
+      g = Math.floor(g / count)
+      b = Math.floor(b / count)
+
+      console.log(`rgb(${r}, ${g}, ${b})`)
+      this.refs.circle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+
+      //const avCnvs = document.querySelector('#average').getContext('2d')
+      //avCnvs.fillStyle = `rgb(${r}, ${g}, ${b})`
+      //avCnvs.fillRect(0, 0, 50, 50)
+
+    })
+  }
+
+  toggleLeftRight = (e) => {
+    const flowLeft = this.state.flowLeft ? false : true
+    const flowRight = this.state.flowRight ? false : true
+    this.setState({flowLeft: flowLeft, flowRight: flowRight})
+  }
+
+  render() {
+    let hej = this.state.flowLeft ? 'body-upload left' : 'body-upload'
+    let hejsan = this.state.flowRight ? 'body-container right' : 'body-container'
+
+    return (
+        <div className='wrapper'>
+          <div className='container'>
+            <div className='header'>
+              <p className='option upload' onClick={this.toggleLeftRight}>UPLOAD</p>
+              <p className='option'>RED</p>
+              <p className='option'>GREEN</p>
+              <p className='option'>BLUE</p>
+            </div>
+            <div className='body'>
+            <div className={hej} ref='bodyUploadRef'>
+              <canvas ref='canvas' width='500' height='500'>Du har inte Canvas din buse</canvas>
+              <form>
+                <input type='file' name='file' id='file' className='inputfile' onChange={this.showImage} />
+                <label htmlFor='file' className='file-button option'>Choose a file</label>
+              </form>
+              <div className='circle' ref='circle'></div>
+            </div>
+            <div className={hejsan} ref='bodyContainerRef'>
+              <div className='row'>
+                <div className='image'></div>
+                <div className='image'></div>
+                <div className='image'></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+//import App from './App'
+import FancyCanvas from './FancyCanvas'
 import './index.css'
 
 ReactDOM.render(
-  <App />,
+  <FancyCanvas />,
   document.getElementById('root')
 )
