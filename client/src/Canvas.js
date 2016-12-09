d01480e6df1787890d2331f5ecc2c6f07a82580c
Pär Eriksson <pe222gq@student.lnu.se>
2016-12-09 16:09:57 +0100

    Knuff

--- client/src/Canvas.js
+++ client/src/Canvas.js
@@ -13,26 +13,42 @@ class Canvas extends Component {
     this.setState({context: ctx})
     const imgFileLoader = document.querySelector('#imgInput')
     imgFileLoader.addEventListener('change', this.showImage, false)
-
   }
 
   showImage(e) {
+    const canvas = document.querySelector('canvas')
+    const ctx = canvas.getContext('2d')
     const reader = new FileReader()
     reader.onload = (event) => {
       const img = new Image()
       img.onload = () => {
-        this.state.context.drawImage(img, 0, 0)
+        ctx.drawImage(
+          img,
+          0,
+          0,
+          img.width,
+          img.height,
+          0,
+          0,
+          canvas.width,
+          canvas.height)
       }
       img.src = event.target.result
     }
     reader.readAsDataURL(e.target.files[0])
   }
 
+  checkPixels() {
+    const ctx = this.state.context
+    console.log('Checking pixels...')
+  }
+
   render() {
     return (
       <div>
         <canvas ref="canvas"></canvas><br/>
-        <input id="imgInput" type="file" />
+        <input id="imgInput" type="file" onChange={this.showImage} /><br/>
+        <button onClick={this.checkPixels}>Check</button>
       </div>
     );
   }
